385f10781f895cf92056466b7f9b9ae9df47ad3f
Abimael Uanderson <abimael.servicos12dt@gmail.com>
2026-01-28 09:46:35 -0300

    feat: Cria o enum EstagioCrescimento

---
+++ src/main/java/br/com/hortaconnect/api/enums/EstagioCrescimento.java
@@ -0,0 +1,11 @@
+package br.com.hortaconnect.api.enums;
+
+public enum EstagioCrescimento {
+    SEMEADURA,
+    GERMINACAO,
+    CRESCIMENTO_VEGETATIVO,
+    FLORACAO,
+    FRUTIFICACAO,
+    COLHEITA,
+    OUTRO
+}

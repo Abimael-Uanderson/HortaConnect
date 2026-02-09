d9af012ab5ef121630138cc54ab39f24b5c040ab
Abimael Uanderson <abimael.servicos12dt@gmail.com>
2026-02-08 22:22:26 -0300

    feat: Cria AgendaCuidadoMapper

---
+++ src/main/java/br/com/hortaconnect/api/mapper/AgendaCuidadoMapper.java
@@ -0,0 +1,37 @@
+package br.com.hortaconnect.api.mapper;
+
+import br.com.hortaconnect.api.dto.AgendaCuidadoRequestDTO;
+import br.com.hortaconnect.api.dto.AgendaCuidadoResponseDTO;
+import br.com.hortaconnect.api.entity.AgendaCuidado;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AgendaCuidadoMapper {
+
+    public AgendaCuidadoResponseDTO toDTO(AgendaCuidado entity) {
+        AgendaCuidadoResponseDTO dto = new AgendaCuidadoResponseDTO();
+
+        dto.setId(entity.getId());
+        dto.setTipoCuidado(entity.getTipoCuidado());
+        dto.setDataAgendamento(entity.getDataAgendamento());
+        dto.setDescricao(entity.getDescricao());
+        dto.setStatusCuidado(entity.getStatusCuidado());
+        
+        if (entity.getPlantio() != null) {
+            dto.setPlantioId(entity.getPlantio().getId());
+            dto.setNomeCultura(entity.getPlantio().getNomeCultura());
+        }
+
+        return dto;
+    }
+
+    public AgendaCuidado toEntity(AgendaCuidadoRequestDTO dto) {
+        AgendaCuidado entity = new AgendaCuidado();
+
+        entity.setTipoCuidado(dto.getTipoCuidado());
+        entity.setDataAgendamento(dto.getDataAgendamento());
+        entity.setDescricao(dto.getDescricao());
+
+        return entity;
+    }
+}
